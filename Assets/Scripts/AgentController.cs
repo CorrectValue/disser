cbcaaf8fe3fd4afe6ddd9f44a63b6b06d816ba3d
Lexa <maythe4bewyou@gmail.com>
2020-03-04 15:58:33 +0400

    Food spawning
    Added a food spawning script and a few models of food to be placed in
    the scene.

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    private int count; //count of collected items
+    private Vector3 destination;
+    private bool wandering;
+   // public UnityEngine.AI.NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        wandering = true;
+        generateRandomPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     //   agent.setDestination(destination);
+    }
+
+    void onTriggerEnter(Collider other)
+    {
+        SphereCollider sCol = other as SphereCollider;
+        //if (other.GameObject.tag == "CollectibleVision")
+        //{
+        //    //spotted a collectible, gotta get it
+        //    destination = sCol.center;
+        //    wandering = false;
+        //}
+        //if (other.GameObject.tag == "collectible")
+        //{
+        //    other.GameObject.setActive(false);
+        //    count += 1;
+        //    wandering = true;
+        //}
+    }
+
+    void generateRandomPoint()
+    {
+        Mesh planeMesh = gameObject.GetComponent<MeshFilter>().mesh;
+        Bounds bounds = planeMesh.bounds;
+
+        float minX = gameObject.transform.position.x - gameObject.transform.localScale.x * bounds.size.x * 0.5f;
+        float minZ = gameObject.transform.position.z - gameObject.transform.localScale.z * bounds.size.z * 0.5f;
+
+        Vector3 newVec = new Vector3(Random.Range(minX, -minX),
+                                     gameObject.transform.position.y,
+                                     Random.Range(minZ, -minZ));
+        destination = newVec;
+    }
+}
+       
---
+++ Assets/Scripts/foodSpawnController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodSpawnController : MonoBehaviour
+{
+
+    private int totalFoodCount;         //a total count of all food items present in the scene
+    private int foodCountThreshold;     //a max number of food in the scene
+    private float nextSpawnTime;        //next spawn time.
+    public float spawnDelay;            //a pause between two consequent spawns
+
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    private Vector3 pos;  //position to spawn to
+    private Quaternion rot;  //rotation to spawn
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime= Time.time + spawnDelay;//
+        var obj;
+        int randNum = Random.Range(0, 1);
+        if(randNum)
+            obj = Instantiate(prefab1, generatePosition(), rot);
+        else
+            obj = Instantiate(prefab2, generatePosition(), rot);
+        //increase counter
+        totalFoodCount++;
+    }
+    
+    private Vector3 generatePosition()
+    {
+        //generates position to spawn to
+        float newx, newy, newz;
+        //generate x, y and z accordingly to the value set by previous methods
+        //TEMPORARILY
+        newx = Random.Range(0, 10);
+        newy = 2;
+        newz = Random.Range(0, 10);
+
+        return new Vector3(newx, newy, newz);
+    }
+
+    private bool shouldSpawn()
+    {
+        //returns true if a certain type of artifact should spawn
+        //common artifact
+        //check if current number of objects is less than its threshold
+        if (totalFoodCount < foodCountThreshold)
+        {
+            //check timer
+            return Time.time >= nextSpawnTime;
+        }
+        else
+        {
+            //total number of objects is equal to the threshold, no need to spawn more
+            //update the timer
+            nextSpawnTime = Time.time + spawnDelay;
+            return false;
+        }
+    }
+}
