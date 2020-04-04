cd291a1a4d00f0bb640a63901dd7c6642ab303cb
Lexa <maythe4bewyou@gmail.com>
2020-04-04 16:52:54 +0400

    More scene management and stolen methods
    Today I added camera movement in the main scene using a free asset from
    Asset Store. Then I added GUI to the main scene, but it doesn't work
    correctly by now.

--- Assets/Scripts/Controlscripts/timer.cs
+++ Assets/Scripts/Controlscripts/timer.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class timer : MonoBehaviour
 {
 
-    public float simulationTime; 
+    public float simulationTime;
+    private Text timerText;
 
     // Start is called before the first frame update
     void Start()
     {
         //set sim time
         simulationTime = 10; //temporarily
+        //find text object to set text into
+        timerText = GameObject.Find("timerText").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -20,6 +24,9 @@ public class timer : MonoBehaviour
     {
         //update sim time 
         simulationTime -= Time.deltaTime;
+        //before showing time to user, need to convert it into more suitable format
+
+        timerText.text = simulationTime.ToString();
 
         if(simulationTime < 0)
         {
@@ -32,11 +39,31 @@ public class timer : MonoBehaviour
     {
         //the simulation stops and the next scene must be loaded
         //thus, we need to store all the data to use it later
+
+        //and switch the scene
+        switchScene();
     }
 
     void switchScene()
     {
         //after everything has been stored, go to the last scene
         SceneManager.LoadScene("Scenes/endMenu");
+    }
+
+    static void GenTimeSpanFromSeconds(double seconds)
+    {
+        //need a method to convert time from float to actual minutes and seconds
+        // Create a TimeSpan object and TimeSpan string from 
+        // a number of seconds.
+        TimeSpan interval = TimeSpan.FromSeconds(seconds);
+        string timeInterval = interval.ToString();
+
+        // Pad the end of the TimeSpan string with spaces if it 
+        // does not contain milliseconds.
+        int pIndex = timeInterval.IndexOf(':');
+        pIndex = timeInterval.IndexOf('.', pIndex);
+        if (pIndex < 0) timeInterval += "        ";
+
+        Console.WriteLine("{0,21}{1,26}", seconds, timeInterval);
     }
 }
