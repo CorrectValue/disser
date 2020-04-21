60e03f301ba55469f896ebbe518893d2c6aa4755
Lexa <maythe4bewyou@gmail.com>
2020-04-21 12:59:47 +0400

    Added death to agents
    Today I added an ability to die for agents. Also, I've tested their
    endurance and how long can they take without foor or water with formulas
    I'm stuck by now.
    And I've found a rather simple realisation of BTs that will find its use
    in this project.
    --------
    Added:
    - deaths
    Tested:
    - time it takes for agents to decrease from 100% HP to 0
    Next up:
    - behavior trees
    - health/sat/hyd bars above heads
    - bars in the main scene UI
    - more UI: ability for camera to follow a selected agent
    - agent sight
    - agent movement control

--- Assets/Scripts/Controlscripts/agentStateController.cs
+++ Assets/Scripts/Controlscripts/agentStateController.cs
@@ -18,7 +18,9 @@ public class agentStateController : MonoBehaviour
     private float maxHealth; 
     private float maxSatiety;
     private float maxHydration;
-    
+
+    //flag that an agent is dead
+    private bool dead;
 
     //thresholds for values above - different for different populations
     private float satietyThreshold;
@@ -32,7 +34,7 @@ public class agentStateController : MonoBehaviour
         health = 100.0f;
         satiety = 70.0f;
         hydration = 70.0f;
-
+        dead = false;
         //initialize thresholds
         //we have a unified interface which means it's urgent to get the type of the population from a different script
         //here, clever agent acts like a balanced one and has somewhat balanced thresholds
@@ -66,63 +68,70 @@ public class agentStateController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //recalculate values and check if they are decreased below turning point
-        //recaltulate satiety based only on time
-        satiety = satiety - Time.deltaTime * 1f; //need to make a better formula
-        if (satiety < 0)
-            satiety = 0;
-        //recalculate hydration based on time - but with different coefficients
-        hydration = hydration - Time.deltaTime * 1.2f; //for now
-        if (hydration < 0)
-            hydration = 0;
-
-        //if the values of satiety and hydration are above their thresholds, restore HP
-        if(hydration > hydrationThreshold && satiety > satietyThreshold)
+        if (!dead)
         {
-            //restore HP if needed
-            if(health < maxHealth)
+            //recalculate values and check if they are decreased below turning point
+            //recaltulate satiety based only on time
+            satiety = satiety - Time.deltaTime * 1f; //need to make a better formula
+            if (satiety < 0)
+                satiety = 0;
+            //recalculate hydration based on time - but with different coefficients
+            hydration = hydration - Time.deltaTime * 1.2f; //for now
+            if (hydration < 0)
+                hydration = 0;
+
+            //if the values of satiety and hydration are above their thresholds, restore HP
+            if (hydration > hydrationThreshold && satiety > satietyThreshold)
             {
-                health += Mathf.Sqrt(Mathf.Pow((hydration - hydrationThreshold), 2) + Mathf.Pow((satiety - satietyThreshold), 2));
-                if (health > maxHealth)
-                    health = maxHealth;
+                //restore HP if needed
+                if (health < maxHealth)
+                {
+                    health += Mathf.Sqrt(Mathf.Pow((hydration - hydrationThreshold), 2) + Mathf.Pow((satiety - satietyThreshold), 2));
+                    if (health > maxHealth)
+                        health = maxHealth;
+                }
             }
-        }
 
-        //now, check if those values have fallen below the threshold
-        //if so, need to reset priorities
-        if (satiety < satietyThreshold)
-        {
-            //the agent is hungry!
-        }
-        if (hydration < hydrationThreshold)
-        {
-            //the agent is thirsty!
-        }
-        if (health < healthThreshold)
-        {
-            //the agent is dying!
-        }
-        //check if any of the stats has fallen below zero
-        if (health <= 0)
-        {
-            //whoops... 
-            die();
-        }
-        if(satiety == 0)
-        {
-            //very hungry, start decreasing health
-            health = health - Time.deltaTime * 1.3f; //need to make a better formula
-        }
-        if(hydration == 0)
-        {
-            //very thirsty, start decreasing health
-            health = health - Time.deltaTime * 1.5f; //need to make a better formula
+            //now, check if those values have fallen below the threshold
+            //if so, need to reset priorities
+            if (satiety < satietyThreshold)
+            {
+                //the agent is hungry!
+            }
+            if (hydration < hydrationThreshold)
+            {
+                //the agent is thirsty!
+            }
+            if (health < healthThreshold)
+            {
+                //the agent is dying!
+            }
+            //check if any of the stats has fallen below zero
+            if (health <= 0)
+            {
+                //whoops... 
+                die();
+            }
+            if (satiety == 0)
+            {
+                //very hungry, start decreasing health
+                health = health - Time.deltaTime * 1.3f; //need to make a better formula
+            }
+            if (hydration == 0)
+            {
+                //very thirsty, start decreasing health
+                health = health - Time.deltaTime * 1.5f; //need to make a better formula
+            }
         }
     }
 
     void die()
     {
         //if health falls below zero, the agent is dead (or stunned)
+        dead = true;
+        //need to animate it somehow
+        gameObject.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+        gameObject.transform.Translate(0.0f, -1.0f, 0.0f);
     }
 
     public void eat(float value)
